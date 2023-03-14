f0ddcb4e9f0a0f28e6316783057795455f214514
Maximiano Leite <macio.pb.cg@gmail.com>
2023-03-14 10:32:30 -0300

    feat: crud repository

--- src/app.js
+++ src/app.js
@@ -1,33 +1,74 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+let repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { url, title, techs } = request.body;
+  const id = uuid();
+  const repository = {
+    url,
+    title,
+    techs,
+    id,
+    likes: 0
+  }
+  repositories.push(repository)
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const id = request.params.id;
+  const { url, title, techs } = request.body;
+  const repositoryIndex = repositories.findIndex((repo) => repo.id === id);
+  if (repositoryIndex === -1) {
+    return response.status(400).send();
+  }
+  const repositoryUpdated = {
+    id: repositories[repositoryIndex].id,
+    url: url ? url : repositories[repositoryIndex].url,
+    title: title ? title : repositories[repositoryIndex].title,
+    techs: techs ? techs : repositories[repositoryIndex].techs,
+    likes: repositories[repositoryIndex].likes,
+  }
+  repositories[repositoryIndex] = repositoryUpdated;
+  return response.json(repositoryUpdated);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const id = request.params.id;
+  const repositoryIndex = repositories.findIndex((repo) => repo.id === id);
+  if (repositoryIndex === -1) {
+    return response.status(400).send();
+  }
+  repositories = repositories.filter((repo) => repo.id !== id);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const id = request.params.id;
+  const repositoryIndex = repositories.findIndex((repo) => repo.id === id);
+  if (repositoryIndex === -1) {
+    return response.status(400).send();
+  }
+  const repositoryUpdated = {
+    ...repositories[repositoryIndex],
+    likes: repositories[repositoryIndex].likes+1,
+  }
+  repositories[repositoryIndex] = repositoryUpdated;
+  return response.json(repositoryUpdated);
 });
 
 module.exports = app;
